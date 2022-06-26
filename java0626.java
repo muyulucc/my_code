4a242a8f2704839d91d05c011a6eee0fd0371557
muyulucc <107872585+muyulucc@users.noreply.github.com>
2022-06-26 23:26:13 +0800

    Add files via upload

---
+++ java0626.java
@@ -0,0 +1,38 @@
+
+/*
+
+public class java20220625  //public class表示类
+{
+	
+	public static void main(String[] args)  //入口
+	{
+
+	System.out.println("Hello java");
+	}
+}
+*/
+
+/*
+public class java2022062501{
+
+	public static void main(String[] args){
+
+		System.out.println("马 is studing java");  //System 首字母大写 
+
+	}
+}
+*/
+
+public class java0626
+{
+
+	public static void main(String[]args){
+
+		int a;
+		a=10;
+		System.out.println(a);
+		int b,c=22;
+		b=a+c;
+		System.out.println(b);
+	}
+}
