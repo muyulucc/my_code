28f763abf7f8a7a32ea2fd242e2e0a9374797021
muyulucc <107872585+muyulucc@users.noreply.github.com>
2022-06-27 22:58:59 +0800

    Add files via upload

---
+++ java0627.java
@@ -0,0 +1,22 @@
+
+
+public class java0627
+{
+
+	public static void main(String[]args){
+
+		int a;
+		a=100;
+		System.out.println(a+"wj");
+		int b,c=22;
+		b=a+c;
+		System.out.println("ly"+c);
+		float d=3.15f;
+		System.out.println("hm"+c+"100");
+		/*Êä³ö½á¹û 
+		100wj
+		ly22
+		hm22100
+		*/
+	}
+}
