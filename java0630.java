63ee15d1509693b044c21bd1fb5d7ab752d63ee0
muyulucc <107872585+muyulucc@users.noreply.github.com>
2022-06-30 23:08:25 +0800

    Add files via upload

---
+++ java0630.java
@@ -0,0 +1,62 @@
+
+/*
+public class java0630
+{
+
+	public static void main(String[]args){
+
+		int num='a';
+		double d1=80;
+		System.out.println(num);
+		System.out.println(d1);
+		//97
+		//80.0
+	}
+}
+
+
+char      -int-long-float-double
+byte-short-int -long-float-double
+
+
+
+ public class StringToBasic{
+
+ 	public static void main(srting[]args){
+
+ 		int n1=100;
+ 		float f1=1.1F;
+ 		double d1=4.5;
+ 		boolean b1=true;
+ 		String s5="123";
+ 		int num1=Integer.parseInt(s5);
+ 		long num4
+ 	}
+ }
+
+
+
+
+  
+*/
+
+
+
+//基本数据类型转string
+
+
+
+public class java0630{
+	public static void main(String[]args){
+
+	
+		String xm = "小明";
+		int o2=24;
+		String o3 = "二班";
+		Double o4="33";
+
+		System.out.println("姓名\t年龄\t班级\t成绩\n"+xm+"\t"+o2+"\t"+
+			o3+"\t"+o4);
+
+	}
+}
